7b73a30e9fc8a03f41dadb3987cd6981381183f8
Bryan Tlatelpa <bryan.284@hotmail.com>
2020-04-16 10:15:18 -0500

    Agrego modificaciones

--- Bootstrap/js/controller.js
+++ Bootstrap/js/controller.js
@@ -4,12 +4,28 @@ $(document).ready(function(){
     var id;
     
     $.ajax({
-        url: 'http://localhost:8080/ASR-Inventario/Back-End/Usuario/getDash',
+        url: 'http://localhost/ASR-Inventario/Back-End/Usuario/getDash',
         type: 'GET',
         data: { 'token': token2 },
         datatype: "JSON",
         success: function(response){
-
+            var tabla = response;
+            let i = ``;
+            tabla.forEach(tabla => {
+                i += `<tr>
+                <td><a href="modificaciones.html" id="modificar">${id = tabla.inventarynum} </a></td>
+                <td>${tabla.serialnum}</td>
+                <td>${tabla.color}</td>
+                <td>${tabla.brand}</td>
+                <td>${tabla.model}</td>
+                <td>${tabla.area}</td>
+                <td>${tabla.edificio}</td>
+                <td>${tabla.estado}</td>
+                <td> <a href="modificaciones.html"></a> </td>
+                </tr>` 
+                    
+            });
+            $('#myTable').html(i);
         }
     })
 
