ea06b5c988e7b56501301a3125a9d771b406215e
Bryan Tlatelpa <bryants.284@gmail.com>
2020-04-29 21:13:26 -0500

    Se limpio codigo de index y User.js y se valido el error devuelto por el back para el logeo

--- Bootstrap/js/User.js
+++ Bootstrap/js/User.js
@@ -2,11 +2,6 @@ $(document).ready(function () {
     $('#formID').submit(function (e) { 
         e.preventDefault();
 
-        
-        // var payload = {
-        //     userName : $('#userInput').val(),
-        //     contra: $('#contraInput').val()
-        // };
         var userName = $('#userInput').val();
         var contra = $('#contraInput').val();
 
@@ -14,12 +9,10 @@ $(document).ready(function () {
             'userName' : userName,
             'contra': contra
         }; 
-
-        console.log(payload);
-
+        
         $.ajax({
             type: "POST",
-            url: "http://localhost/Proyecto/Back-End/Usuario/login",
+            url: "http://localhost:8080/Proyecto/Back-End/Usuario/login",
             data: payload,
             dataType: "json",
             beforeSend: function(){
@@ -27,84 +20,21 @@ $(document).ready(function () {
               $('#login').css({'display': 'none'});
             },
             success: function (response) {
-                console.log(response);
-                token = response['token'];
-                console.log(token);
-                var token1= localStorage.setItem("tokenPrincipal",token);
-                alert("Funciono");
+              if(response['error'] == 0){
+                localStorage.setItem("tokenPrincipal",response['token']);
                 window.location.href = "dash.html";
+              }else{
+                alert(response['message']);
+                $('#contraInput').val("");
+              }
             },
             error: function(){
-                alert("No Funciono");
+              alert("Error: No se realizar login.");
             },
             complete: function() {
               $('.fa').css('display','none');
               $('#login').css({'display': ''});
             },
         });
-
-       // $('#login').click(function(){
-         //   var esperar = 2;
-           // $.ajax({
-              //  url: "dash.html",
-                //beforeSend : function(){
-       //             $('#seccion').text('');
-         //       },
-
-           //     success : function(data){
-             //       setTimeout(function(){
-               //         $('#seccion').html(data);
-                 //   }, esperar
-                   // );
-              //  }
-          //  });
-        //});
-
-
-        //
-        
-        // var userName = $('#userInput').val();
-        // var contra = $('#contraInput').val();
-    
-        // var payload = {
-        //     name: 'usserName',
-        //     vaue: userName
-        // }; //{name: 'userName' ,value: userName,name: 'contra', value: contra};
-
-        // console.log(payload);
-
-        // $.ajax({
-        //     type: "post",
-        //     url: "http://localhost:8080/Proyecto_ID_2/Back-End/Usuario/login",
-        //     data: payload,
-        //     dataType: "json",
-        //     // beforeSend: function(){
-        //     //     $('.fa').css('display','inline');
-        //     // }
-        // }).done(function(response){
-        //     $('span').html("Correcto");
-        //     console.log(response);
-        // }).fail(function(){
-        //     alert("No funciono");
-        //     //$('span').html("Incorrecto");
-        // }).always(function(){ //Esta parte se ejecuta siempre que se realiza el request
-        //     // $('.fa').hide();
-        // });
-
-        // $.ajax({
-        //     type: "POST",
-        //     url: "http://localhost:8080/Proyecto_ID_2/Back-End/Usuario/login",
-        //     data: payload,
-        //     dataType: "json",
-        //     success: function (response) {
-        //         console.log(reponse);
-        //     },
-        //     error: function(xhr, status, error) {
-        //         //var err = eval("(" + xhr.responseText + ")");
-        //         //alert(err.Message);
-        //         alert("Hay un error.");
-        //     }
-        // });
-        
     });
 });
