91ad1e8e32b4b3969b3e50ff75a5b642a0094915
ItzMont <itz_mont09@outlook.com>
2020-05-13 16:22:21 -0500

    Herramienta buscar funciona

--- Bootstrap/js/controller.js
+++ Bootstrap/js/controller.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     var token2=localStorage.getItem("tokenPrincipal");
     var id;
     
-    
 
     $.ajax({
         url: 'http://localhost/Proyecto/Back-End/Usuario/getDash',
@@ -208,7 +207,23 @@ $(document).ready(function(){
             url: 'http://localhost/Proyecto/Back-End/Usuario/getDash',
             type: 'GET',
             success: function(response){
-                //console.log(response);
+                console.log(response);
+                var input, filter, table, tr, td, i, txtValue;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("myTable");
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {
+                    td = tr[i].getElementsByTagName("td")[0];
+                    if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                    }       
+                }
             }
         })
     })
