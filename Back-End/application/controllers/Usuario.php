d88cb7c72a09245c215fb526dd9eca1f9f22b9fc
Bryan Tlatelpa <bryants.284@gmail.com>
2020-05-14 12:03:30 -0500

    Se busco implementar el creatorPDF pero no se pudo

--- Back-End/application/controllers/Usuario.php
+++ Back-End/application/controllers/Usuario.php
@@ -11,7 +11,7 @@ use Restserver\libraries\REST_Controller_Definitions;
 require APPPATH . '/libraries/REST_Controller_Definitions.php';
 require APPPATH . '/libraries/REST_Controller.php';
 require APPPATH . '/libraries/Format.php';
-//require_once 'CreatorPDF.php';
+// require_once 'CreatorPDF.php';
 
 class Usuario extends CI_Controller{
 
@@ -291,41 +291,45 @@ class Usuario extends CI_Controller{
 
 
 	//=========NO TIENE BUEN FUNCIONAMIENTO PORQUE ES NECESARIO DEFINIR UN DASH
-	/*
-	public function createReportAllProducts_get(){
-		$this->load->model('UsuarioM');
-		// //Forma normal dde recibir los datos
-		// $payload = json_decode($this->input->get('payload'));
-		// $token = $payload->token;
-		//=====================================================
-		//Forma para el Front-End de recuperar la informacion
-		$token = $this->input->get('token');
+	
+	// public function createReportAllProducts_get(){
+	// 	$this->load->model('UsuarioM');
+	// 	// //Forma normal dde recibir los datos
+	// 	// $payload = json_decode($this->input->get('payload'));
+	// 	// $token = $payload->token;
+	// 	//=====================================================
+	// 	//Forma para el Front-End de recuperar la informacion
+	// 	$token = $this->input->get('token');
 		
-		if(!empty($token)){
-			try{
-				$arrOfToken = AUTHORIZATION::validateToken($token);
+	// 	if(!empty($token)){
+	// 		try{
+	// 			$arrOfToken = AUTHORIZATION::validateToken($token);
 	
-				$idUser = $arrOfToken->idUser;
-				$idSesion = $arrOfToken->session;
+	// 			$idUser = $arrOfToken->idUser;
+	// 			$idSesion = $arrOfToken->session;
 	
-				$resultQuery = $this->UsuarioM->GetDash($idUser,$idSesion);
+	// 			$resultQuery = $this->UsuarioM->GetDash($idUser,$idSesion);
 	
-				if(!array_key_exists('error',$resultQuery)){
+	// 			if(!array_key_exists('error',$resultQuery)){
 	
-					$pdf = new CreatorPDF();
+	// 				$pdf = new CreatorPDF();
 	
-					$pdf->createReportAllProducts($resultQuery);
+	// 				$pdf->createReportAllProducts($resultQuery);
 	
-				}else{
-					$resulSet = array("error" => 103, "message" => "Contact the administrator.");
-				}
+	// 				$pdf->Output('D','MyPDF.pdf');
+	// 			}else{
+	// 				$resulSet = array("error" => 103, "message" => "Contact the administrator.");
+	// 			}
 				
-			}catch(Exception $e){
-				$resulSet = array("error" => 104, 'message' => "Error contacte al administrador.");
-			}
-		}else{
-			header('Location: ./../../index.html');
-		}
+	// 		}catch(Exception $e){
+	// 			$resulSet = array("error" => 104, 'message' => "Error contacte al administrador.");
+	// 		}
+	// 	}else{
+	// 		// header('Location: ./../../index.html');
+	// 	}
+
+	// 	// $pdf = new CreatorPDF();
+	// 	// $pdf->createReportAllProducts('hola');
 
 
 
@@ -334,21 +338,20 @@ class Usuario extends CI_Controller{
 
 
 
-
-		//=======
-		// $resultQuery = $this->UsuarioM->GetDash(1,5);
+	// 	//=======
+	// 	// $resultQuery = $this->UsuarioM->GetDash(1,5);
 	
-		// if(!array_key_exists('error',$resultQuery)){
+	// 	// if(!array_key_exists('error',$resultQuery)){
 
-		// 	$pdf = new CreatorPDF();
+	// 	// 	$pdf = new CreatorPDF();
 
-		// 	$pdf->createReportAllProducts($resultQuery);
+	// 	// 	$pdf->createReportAllProducts($resultQuery);
 
-		// }else{
-		// 	$resulSet = array("error" => 103, "message" => "Contact the administrator.");
-		// }
+	// 	// }else{
+	// 	// 	$resulSet = array("error" => 103, "message" => "Contact the administrator.");
+	// 	// }
 
 		
 		
-	}*/
+	// }
 }
